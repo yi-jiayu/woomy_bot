ee5f9a567c7e3437fc15cb1c74a4194dcb19690a
Jiayu Yi <yijiayu@gmail.com>
2022-01-03 21:55:02 +0800

    Use shorter timestamps for lobby rotations

--- main.py
+++ main.py
@@ -13,33 +13,35 @@ app = FastAPI()
 sgt = timezone(timedelta(hours=8))
 
 
-def formatted_time(t: datetime, tz):
-    return t.astimezone(tz).strftime("%a, %b %d, %I %p")
-
-
 def time_until_start_or_end_msg(start_time, end_time, now):
+    parts = []
     if start_time < now:
         dt = end_time - now
-        verb = 'Ends'
+        parts.append(f'Ends in')
     else:
         dt = start_time - now
-        verb = 'Starts'
-    days = dt.days
-    hours = dt.seconds // 3600
-    minutes = (dt.seconds % 3600) // 60
-    return f'{verb} in {days} d {hours} h {minutes} m'
+        parts.append(f'Starts in')
+    if days := dt.days:
+        parts.append(f'{days} d')
+    if hours := dt.seconds // 3600:
+        parts.append(f'{hours} h')
+    if minutes := (dt.seconds % 3600) // 60:
+        parts.append(f'{minutes} m')
+    return ' '.join(parts)
 
 
 def build_salmon_result(rota: SalmonRotation, t):
     msg = time_until_start_or_end_msg(rota.start_time, rota.end_time, t)
+    start_time = rota.start_time.astimezone(sgt).strftime("%a, %b %d, %I %p")
+    end_time = rota.end_time.astimezone(sgt).strftime("%a, %b %d, %I %p")
     return {
         "type": "article",
         "id": f'Salmon Run {rota.start_time.timestamp()}',
         "title": f'Salmon Run: {rota.stage}',
-        "description": f'{formatted_time(rota.start_time, sgt)} - {formatted_time(rota.end_time, sgt)}\n{", ".join(rota.weapons)}',
+        "description": f'{start_time} - {end_time}\n{", ".join(rota.weapons)}',
         "input_message_content": {
             "message_text": f"""<strong>{rota.stage}</strong>
-{formatted_time(rota.start_time, sgt)} - {formatted_time(rota.end_time, sgt)}
+{start_time} - {end_time}
 {msg}
 {', '.join(rota.weapons)}""",
             "parse_mode": "HTML",
@@ -50,14 +52,16 @@ def build_salmon_result(rota: SalmonRotation, t):
 
 def build_lobby_result(title, rota: LobbyRotation, t):
     msg = time_until_start_or_end_msg(rota.start_time, rota.end_time, t)
+    start_time = rota.start_time.astimezone(sgt).strftime('%I %p')
+    end_time = rota.end_time.astimezone(sgt).strftime('%I %p')
     return {
         "type": "article",
         "id": f'{title} {rota.start_time.timestamp()}',
         "title": title,
-        "description": f'{formatted_time(rota.start_time, sgt)} - {formatted_time(rota.end_time, sgt)}\n{", ".join(rota.stages)}',
+        "description": f'{start_time} - {end_time}\n{", ".join(rota.stages)}',
         "input_message_content": {
             "message_text": f"""<strong>{title}</strong>
-{formatted_time(rota.start_time, sgt)} - {formatted_time(rota.end_time, sgt)}
+{start_time} - {end_time}
 {msg}
 {', '.join(rota.stages)}""",
             "parse_mode": "HTML",
